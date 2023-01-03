13b5813986bc40b26cae687e0406abc1b87e7548
Jim__TT <1820452084@qq.com>
2023-01-03 17:58:38 +0800

    更新TabBar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-
+    <router-view/>
+    <main-tar-bar/>
   </div>
 </template>
 
 <script>
 
+  import MainTarBar from './components/content/maintarbar/MainTarBar';
+
 export default {
   name: 'App',
   components: {
-  
+    MainTarBar
   }
 }
 </script>
-  
-<style>
 
+<style>
+  @import "assets/css/base.css";
 </style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="home">
+    <nav-bar></nav-bar>
+  </div>
+</template>
+
+<script>
+
+  import NavBar from 'components/common/navbar/NavBar';
+
+export default {
+  name: "Home",
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+
+</style>
